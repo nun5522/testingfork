62f089742afbf5a061d6ee7e4744acf118b89cc1
nuntawat <nuntawat.sorn@bumail.net>
2024-12-02 13:58:03 +0700

    fix namespace

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Project.GameManager;
+namespace Project.GameManager
 {
     public class HealthManager : MonoBehaviour
     {
-        pubilc int IncreaseHealth(int currentHealth, int increseAmount, int maxHealth)
+        public int IncreaseHealth(int currentHealth, int increseAmount, int maxHealth)
         {
         int newHealth = currentHealth + increseAmount;
         return Mathf.Clamp(newHealth, 0, maxHealth);
