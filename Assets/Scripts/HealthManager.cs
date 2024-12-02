2f6fe1ebe4d8bfe1304d2a6d7fbebcb39f41cefa
nuntawat <nuntawat.sorn@bumail.net>
2024-12-02 13:46:30 +0700

    new hp script

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -2,24 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthManager : MonoBehaviour
+namespace Project.GameManager;
 {
-    pubilc int IncreaseHealth(int currentHealth, int increseAmount, int maxHealth)
+    public class HealthManager : MonoBehaviour
     {
+        pubilc int IncreaseHealth(int currentHealth, int increseAmount, int maxHealth)
+        {
         int newHealth = currentHealth + increseAmount;
-        return ClampHealth(currentHealth, newHealth);
-    }
-
-    public int ClampHealth(int currentHealth, int newHealth)
-    {
-        if (currentHealth > maxHealth)
-        {
-            return maxHealth;
-        }
-        else
-        {
-            return currentHealth;
+        return Mathf.Clamp(newHealth, 0, maxHealth);
         }
     }
-    
 }
